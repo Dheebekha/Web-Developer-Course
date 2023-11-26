c2a732a5cda365ab5b1cd68ff6491cf30d1d4063
Dheebekha <vcdeepi@gmail.com>
2023-11-26 18:45:47 +0100

    Bootstrap

---
+++ .vscode/Javascript/calc.js
@@ -0,0 +1,38 @@
+
+
+function calc(option){
+    let a = document.getElementById("first").value
+   let b = document.getElementById("second").value
+   let output;
+   if(option == 'add'){
+    output = `Sum of the number is ${(Number(a) + Number(b))}`
+   }
+   else
+   {
+     output = `Sub of the number is ${(Number(a) - Number(b))}`
+   }
+   document.getElementById("out").innerText = output
+}
+
+
+
+
+
+
+
+/* function add()
+{
+   let a = document.getElementById("first").value
+   let b = document.getElementById("second").value
+   let output = `Sum of the number is ${(Number(a) + Number(b))}`
+   document.getElementById("out").innerText = output
+}
+
+
+function sub()
+{
+   let a = document.getElementById("first").value
+   let b = document.getElementById("second").value
+   let output = `Sub of the number is ${(Number(a) - Number(b))}`
+   document.getElementById("out").innerText = output
+} */
